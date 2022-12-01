a1606110d011e72e5840f2e7ab93aa44cc7682d8
Jean-Baptiste CAMBILLARD <jeanbaptiste.cambillard@gmail.com>
2022-12-01 11:20:42 +0100

    Initial Commit

---
+++ Fold V1/Fold_V1App.swift
@@ -0,0 +1,17 @@
+//
+//  Fold_V1App.swift
+//  Fold V1
+//
+//  Created by JB on 01/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Fold_V1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
